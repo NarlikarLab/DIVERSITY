833f3ad836734777d53c51e7d15af0e080cf15b0
Sneha Mitra <sneha@cs.duke.edu>
2016-12-30 18:03:08 -0500

    Updated mumodMain.py

--- mumod13_lookAhead_addOns/mumodMain.py
+++ mumod13_lookAhead_addOns/mumodMain.py
@@ -4,6 +4,28 @@ from getParams import *
 import evaluate as ev
 import saveFiles as sf
 
+# save settings in file
+
+def saveSettings(d):
+    f = open(d['-o'][1] + "/" + "settings.txt", "w")
+    f.write("\nInput fasta file: " + os.path.abspath(d['-f']))
+    f.write("\n\nOutput prefix: " + os.path.abspath(d['-o'][1]))
+    f.write("\n\nMinimum number of modes: " + str(d['-minMode']))
+    f.write("\n\nMaximum number of modes: " + str(d['-maxMode']))
+    f.write("\n\nNumber of models to be learnt per mode: " + str(d['-lcount']))
+    f.write("\n\nMinimum motif width: " + str(d['-minWidth']))
+    f.write("\n\nInitial motif width: " + str(d['-initialWidth']))
+    f.write("\n\nMask repeats: ")
+    if (d['-maskReps'] == 1): f.write("Yes")
+    else: f.write("No")
+    f.write("\n\nInclude reverse strand: ")
+    if (d['-r'] == 1): f.write("Yes")
+    else: f.write("No")
+    f.write("\n\nZOOPS: " + str(d['-zoops']))
+    f.write("\n\nMaximum number of cores used: " + str(d['-proc']))
+    f.write("\n")
+    f.close()
+    
 # print execution information
 def printDetails(d):
     print "\n\nProcessing details:\n"
@@ -16,6 +38,7 @@ def printDetails(d):
     print "Output file consisting of motif positions and strand information would be saved as " + d['-o'][1] + "/info.txt for each mode"
     print "Sequence logos for all motifs of every mode would be saved in each mode directory"
     print "PSSMs for all motifs of every mode would be saved as " + d['-o'][1] + "/pssm.txt"
+    print("Execution settings will be saved as " + d['-o'][1] + "/" + "settings.txt")
     print "\n\n"
 
 # function that calls the training function and then calls another function to save details
@@ -25,6 +48,7 @@ def getModel(d):
     if d['-proc'] < n and d['-proc'] > 0: n = d['-proc']
     d['-proc'] = n
     printDetails(d)
+    saveSettings(d)
     to = ev.learn(d)
     print "\n\nSaving details..."
     sf.saveDetails(d, to)
