2e0159ce1e41934a70973572bfb7e922196fbee1
Sneha Mitra <sneha@cs.duke.edu>
2017-01-24 18:44:08 -0500

    Added license

--- saveFiles.py
+++ saveFiles.py
@@ -1,3 +1,26 @@
+##################### DIVERSITY #####################
+
+#    DIVERSITY is a tool to explore multiple ways of protein-DNA
+#    binding in the genome. More information can be found in the README file.
+#    Copyright (C) 2015  Sneha Mitra, Anushua Biswas and Leelavati Narlikar
+
+#    DIVERSITY is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    DIVERSITY is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+######################################################
+
+# Save DIVERSITY output files
+
 import weblogoMod.weblogolib as wl
 import numpy as np
 import pickle
